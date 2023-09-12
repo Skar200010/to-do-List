70be143d3dc6b7ff4d2845b92eed021a6fd0584b
Skar200010 <khedekarsohan10@gmail.com>
2023-09-12 14:08:44 +0530

    adding some extra validations

--- To-Do-List/middlewares/taskMiddleware.js
+++ To-Do-List/middlewares/taskMiddleware.js
@@ -1,7 +1,18 @@
 const { body, validationResult } = require('express-validator');
+const Task = require('../models/Task'); // Import your Task model
 
 const createTaskValidation = [
-  body('taskName').notEmpty().withMessage('Task name is required'),
+  body('taskName')
+    .notEmpty()
+    .withMessage('Task name is required')
+    .custom(async (value, { req }) => {
+      // Check if the task name is already in use
+      const existingTask = await Task.findOne({ taskName: value });
+      if (existingTask) {
+        throw new Error('Task name must be unique');
+      }
+      return true;
+    }),
   body('taskDescription').notEmpty().withMessage('Task description is required'),
   body('listId').notEmpty().withMessage('List ID is required'),
   (req, res, next) => {
@@ -12,6 +23,5 @@ const createTaskValidation = [
     next();
   },
 ];
-
 
 module.exports = { createTaskValidation };
